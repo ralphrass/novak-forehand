95e76eccf6b76fecafc674152b327aea5393a38f
ralphrass <ralphrass@gmail.com>
2024-12-20 01:54:19 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,37 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    env: {
-      NEXT_PUBLIC_AZURE_OPENAI_KEY: process.env.NEXT_PUBLIC_AZURE_OPENAI_KEY,
-      NEXT_PUBLIC_AZURE_OPENAI_ENDPOINT: process.env.NEXT_PUBLIC_AZURE_OPENAI_ENDPOINT,
-    },
-    reactStrictMode: true,
-    compress: true,
-    images: {
-      domains: ['novakstorage.blob.core.windows.net'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**.windows.net',
-          port: '',
-          pathname: '/files/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_AZURE_OPENAI_KEY: process.env.NEXT_PUBLIC_AZURE_OPENAI_KEY,
+    NEXT_PUBLIC_AZURE_OPENAI_ENDPOINT: process.env.NEXT_PUBLIC_AZURE_OPENAI_ENDPOINT,
+  },
+  reactStrictMode: true,
+  compress: true,
+  images: {
+    domains: ['novakstorage.blob.core.windows.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.windows.net',
+        port: '',
+        pathname: '/files/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };    
+    return config;
+  },
+};
+
+export default nextConfig;
