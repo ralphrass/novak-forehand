f754d27014c3d828664600b7a55824af45821e9d
ralphrass <ralphrass@gmail.com>
2024-12-21 10:25:30 -0300

    fix: following frontman steps

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,9 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   env: {
     NEXT_PUBLIC_AZURE_OPENAI_KEY: process.env.NEXT_PUBLIC_AZURE_OPENAI_KEY,
     NEXT_PUBLIC_AZURE_OPENAI_ENDPOINT: process.env.NEXT_PUBLIC_AZURE_OPENAI_ENDPOINT,
   },
-  reactStrictMode: true,
-  compress: true,
   images: {
     domains: ['novakstorage.blob.core.windows.net'],
     remotePatterns: [
@@ -25,13 +15,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    };    
-    return config;
-  },
-};
+}
 
-export default nextConfig;
+export default nextConfig
