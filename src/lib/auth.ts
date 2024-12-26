3e0a0a981d40aca08ce4b75e22cd1bac653e05d6
ralphrass <ralphrass@gmail.com>
2024-12-26 11:22:15 -0300

    auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,27 @@
+// src/lib/auth.ts
+import type { NextAuthOptions } from "next-auth";
+import AzureADProvider from "next-auth/providers/azure-ad";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID!,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+      tenantId: "common",
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
+      httpOptions: {
+        timeout: 10000,
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  debug: true,
+};
